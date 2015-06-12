8cb27479b9c4ab336e6ebb2a3aab9fc0ef88660a
Shwetabh Sharan <shwetabhs@saltstack.com>
2015-06-12 16:51:51 +0530

    Covered $http, push, post call, show updated contetnt on the ui

--- CRUD/controller.js
+++ CRUD/controller.js
@@ -2,4 +2,13 @@ app.controller("crudController", function($scope, crudService){
 	crudService.listEntries().then(function(data){
 		$scope.response_data = data.records;
 	});
+
+	$scope.created_data = [];
+	$scope.createEntryButton = function(){
+		var pub_data = $scope.pub_data;
+		crudService.createEntry(pub_data).then(function(data){
+			$scope.created_data.push(data);
+			console.log($scope.created_data);
+		});
+	}
 });
--- CRUD/service.js
+++ CRUD/service.js
@@ -10,4 +10,17 @@ app.service("crudService", function($http, $q){
 			});
 		return deferred.promise;
 	};
+
+	this.createEntry = function(pub_data){
+		var deferred = $q.defer();
+		$http.post('http://jsonplaceholder.typicode.com/posts', pub_data).
+		success(function(data){
+			deferred.resolve(data);
+		}).
+		error(function(data){
+			deferred.reject(data);
+		});
+		console.log(deferred.promise);
+		return deferred.promise;
+	};
 });
